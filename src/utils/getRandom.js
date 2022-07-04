8fe9fe9b9a4c949b1e7890181812f4f78c82050c
duskStar <dusk.star@foxmail.com>
2022-07-04 11:35:43 +0800

    add some helper functions

---
+++ src/utils/getRandom.js
@@ -0,0 +1,44 @@
+/**
+ * Generate random data
+ * 
+ * @param {*} min Minimum value or length
+ * @param {*} max Maximum value or length
+ * @param {string} type Desired data type, support: string、number
+ */
+export default function(min=0, max=100, type="number") {
+
+    function numRandom(min, max) {
+        return Math.round(min + Math.random() * (max - min));
+    }
+    
+    function strRandom() {
+        let string = '',
+            long = numRandom(min, max),
+            list = [
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
+                'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+                'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+',
+                '<', '>', '?', ',', '.', '/', '|', '`', '"', "'", ';', ':',
+                '、',' ', '[', ']', '{', '}'
+            ];
+        
+        for(var i = 0; i < long; i++) {
+            var oLong = numRandom(0, list.length - 1);
+            string += list[oLong];
+        }
+        return string
+    }
+
+    switch(type) {
+        case "number":
+            return numRandom(min, max);
+        case "string":
+            return strRandom();
+        default:
+            throw new Error("please input a valid data type");
+    }
+}
