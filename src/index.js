fa87e4ba54799d44a2a3942e97aa13354e9059e3
duskStar <dusk.star@foxmail.com>
2022-07-08 14:46:05 +0800

    enhance getRandom helper function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import cookies from './utils/Imitate/cookies.js';
 import popMessage from './utils/popMessage.js';
+import getRandom from './utils/getRandom.js';
 
 window.cookies = cookies;
 window.popMessage = popMessage;
+window.getRandom = getRandom;
